e55f8ac012f2eb37dcbc959dd95e96b30abe767b
Tom Dooner <tom.dooner@brigade.com>
2016-09-05 16:43:55 -0700

    Serve Access Control header

--- server.rb
+++ server.rb
@@ -2,5 +2,8 @@ require 'sinatra'
 
 get '*' do |path|
   file = File.expand_path('../build' + path + '/index.json', __FILE__)
+  headers(
+    'Access-Control-Allow-Origin' => '*',
+  )
   send_file(file)
 end
