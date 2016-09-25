7dd475bbf924d9f8b21ad3d46aa1d5cf295665c7
Tom Dooner <tom.dooner@brigade.com>
2016-09-25 14:18:00 -0700

    Add calculator for committee contribution list
    This calculator, if my queries are sound, combines 460 schedules A and C
    with 497 page 1 contributions into a giant list accessible at the API
    endpoint:
    
      /committee/:filer_id/contributions
    
    Eventually we will need to de-dupe the 497 contributions out of there.

---
+++ calculators/committee_contribution_list_calculator.rb
@@ -0,0 +1,50 @@
+class CommitteeContributionListCalculator
+  def initialize(candidates: [], ballot_measures: [], committees: [])
+    @committees = committees
+  end
+
+  def fetch
+    results = ActiveRecord::Base.connection.execute(<<-SQL)
+      -- Schedule A Monetary Contributions
+      SELECT "Filer_ID"::varchar, "Tran_Amt1", "Tran_NamF", "Tran_NamL", "Tran_Date"
+      FROM "efile_COAK_2016_A-Contributions"
+      WHERE "Filer_ID"::varchar IN (#{filer_ids})
+      UNION
+
+      -- Schedule C In-Kind contributions
+      SELECT "Filer_ID"::varchar, "Tran_Amt1", "Tran_NamF", "Tran_NamL", "Tran_Date"
+      FROM "efile_COAK_2016_C-Contributions"
+      WHERE "Filer_ID"::varchar IN (#{filer_ids})
+      UNION
+
+      -- Form 497 Late Contributions
+      -- TODO: we will need to dedupe this against the other forms
+      SELECT
+        "Filer_ID"::varchar,
+        "Amount" AS "Tran_Amt1",
+        "Enty_NamF" AS "Tran_NamF",
+        "Enty_NamL" AS "Tran_NamL",
+        "Ctrib_Date" AS "Tran_Date"
+      FROM "efile_COAK_2016_497"
+      WHERE "Form_Type" = 'F497P1'
+      AND "Filer_ID"::varchar IN (#{filer_ids})
+    SQL
+
+    contributions_by_committee = results.each_with_object({}) do |row, hash|
+      filer_id = row['Filer_ID'].to_s
+
+      hash[filer_id] ||= []
+      hash[filer_id] << row
+    end
+
+    @committees.each do |committee|
+      filer_id = committee['Filer_ID'].to_s
+
+      committee.save_calculation(:contribution_list, contributions_by_committee[filer_id])
+    end
+  end
+
+  def filer_ids
+    @committees.map(&:Filer_ID).map { |f| "'#{f}'::varchar" }.join(',')
+  end
+end
