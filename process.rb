3dd27fc85946799ef4dabe7f6a397f8dfb309824
Tom Dooner <tom.dooner@brigade.com>
2016-09-28 22:17:00 -0700

    Remove the swagger stuff

--- process.rb
+++ process.rb
@@ -130,17 +130,3 @@ OaklandReferendum.find_each do |referendum|
     ))
   end
 end
-
-build_file('/docs/api-docs/') do |f|
-  spec = JSON.parse(open('http://admin.caciviclab.org/docs/api-docs/').read)
-  spec['basePath'] = 'http://disclosure-backend-static.f.tdooner.com/docs/api-docs/'
-  f.puts JSON.pretty_generate(spec)
-
-  spec['apis'].each do |api_resource|
-    build_file("/docs/api-docs/#{api_resource['path']}") do |f2|
-      resource_spec = JSON.parse(open("http://admin.caciviclab.org/docs/api-docs/#{api_resource['path']}").read)
-      resource_spec['basePath'] = 'http://disclosure-backend-static.f.tdooner.com'
-      f2.puts JSON.pretty_generate(resource_spec)
-    end
-  end
-end
