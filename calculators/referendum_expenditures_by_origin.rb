3cb0c0f13e19255b5da11c97cfc9e3647ad6b926
Mike Ubell <mike.ubell@gmail.com>
2016-10-01 14:16:46 -0700

    Fix #42 -- add Schedule C to ballot measure contributions

--- calculators/referendum_expenditures_by_origin.rb
+++ calculators/referendum_expenditures_by_origin.rb
@@ -38,13 +38,16 @@ class ReferendumExpendituresByOrigin
         END AS locale,
         SUM("Tran_Amt1") AS total
         FROM (
-          SELECT "Filer_ID", "Tran_City", "Tran_State", "Tran_Amt1"
+          SELECT "Filer_ID", "Tran_City", "Tran_State", "Tran_Amt1", "Tran_ID"
           FROM "efile_COAK_2016_A-Contributions"
+          UNION
+          SELECT "Filer_ID"::varchar, "Tran_City", "Tran_State", "Tran_Amt1", "Tran_ID"
+          FROM "efile_COAK_2016_C-Contributions"
           UNION
           SELECT "Filer_ID"::varchar,
             "Enty_City" as "Tran_City",
             "Enty_ST" as "Tran_State",
-            "Amount" as "Tran_Amt1"
+            "Amount" as "Tran_Amt1", "Tran_ID"
           FROM "efile_COAK_2016_497"
           WHERE "Form_Type" = 'F497P1'
         ) contributions
