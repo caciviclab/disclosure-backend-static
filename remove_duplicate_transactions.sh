76cd020322d00b6bc979f69ef5f9c2bc8f4b81ff
Tom Dooner <tom.dooner@brigade.com>
2016-10-04 20:51:34 -0700

    Dedupe more liberally everywhere and also on C/497

--- remove_duplicate_transactions.sh
+++ remove_duplicate_transactions.sh
@@ -3,27 +3,36 @@ set -euo pipefail
 
 # 1. delete duplicate A/497 contributions
 cat <<-QUERY | psql disclosure-backend
-DELETE FROM "efile_COAK_2016_497"
+DELETE FROM "efile_COAK_2016_497" late
 USING "efile_COAK_2016_A-Contributions" contributions
-WHERE contributions."Filer_ID"::varchar = "efile_COAK_2016_497"."Filer_ID"::varchar
-AND contributions."Tran_ID" = "efile_COAK_2016_497"."Tran_ID"
-AND "efile_COAK_2016_497"."Form_Type" = 'F497P1';
+WHERE contributions."Filer_ID"::varchar = late."Filer_ID"::varchar
+AND (
+  contributions."Tran_ID" = late."Tran_ID"
+  OR (LOWER(contributions."Tran_NamL") = LOWER(late."Enty_NamL") AND contributions."Tran_Amt1" = late."Amount")
+)
+AND late."Form_Type" = 'F497P1';
 QUERY
 
 # 2. delete duplicate C/497 contributions
 cat <<-QUERY | psql disclosure-backend
-DELETE FROM "efile_COAK_2016_497"
+DELETE FROM "efile_COAK_2016_497" late
 USING "efile_COAK_2016_C-Contributions" contributions
-WHERE contributions."Filer_ID"::varchar = "efile_COAK_2016_497"."Filer_ID"::varchar
-AND contributions."Tran_ID" = "efile_COAK_2016_497"."Tran_ID"
-AND "efile_COAK_2016_497"."Form_Type" = 'F497P1';
+WHERE contributions."Filer_ID"::varchar = late."Filer_ID"::varchar
+AND (
+  contributions."Tran_ID" = late."Tran_ID"
+  OR (LOWER(contributions."Tran_NamL") = LOWER(late."Enty_NamL") AND contributions."Tran_Amt1" = late."Amount")
+)
+AND late."Form_Type" = 'F497P1';
 QUERY
 
 # 3. delete duplicate E/497 expenditures
 cat <<-QUERY | psql disclosure-backend
-DELETE FROM "efile_COAK_2016_497"
+DELETE FROM "efile_COAK_2016_497" late
 USING "efile_COAK_2016_E-Expenditure" expenditures
-WHERE expenditures."Filer_ID"::varchar = "efile_COAK_2016_497"."Filer_ID"::varchar
-AND expenditures."Tran_ID" = "efile_COAK_2016_497"."Tran_ID"
-AND "efile_COAK_2016_497"."Form_Type" = 'F497P2';
+WHERE expenditures."Filer_ID"::varchar = late."Filer_ID"::varchar
+AND (
+  expenditures."Tran_ID" = late."Tran_ID"
+  OR (LOWER(expenditures."Payee_NamL") = LOWER(late."Enty_NamL") AND expenditures."Amount" = late."Amount")
+)
+AND late."Form_Type" = 'F497P2';
 QUERY
