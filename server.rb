8e17ce2b6e5d9ac8c8dc9bc0be1ef048676e78be
Tom Dooner <tom.dooner@brigade.com>
2016-11-30 15:14:24 -0500

    Add simple page at / for health-checking reasons
    I'm addressing some infrastructure debt on my personal server, and I'd
    like to add a health check of this app. But it feels weird to have the
    health-check alerting based on the /ballot/1 path (or anything
    application-specific).
    
    I also just accidentally thought the site was down by going to / and
    seeing nothing. Turned out it was just a 404. To prevent accidents like
    this in the future, I'm making the root URL return something arbitrary.

--- server.rb
+++ server.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
+# Make the root URL not a 404 so it's not confusing whether the site is up or
+# down.
+get '/' do
+  url = 'https://github.com/caciviclab/disclosure-backend-static/tree/master/build'
+  "<a href='#{url}'>View list of API endpoints</a>"
+end
+
 get '*' do |path|
   file = File.expand_path('../build' + path + '/index.json', __FILE__)
   headers(
