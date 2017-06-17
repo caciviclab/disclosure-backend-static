3e71b7b60a61f0fe479da9c1e0777a41996274eb
Tom Dooner <tdooner@codeforamerica.org>
2017-06-17 13:41:49 -0700

    Add script to provision machine and travis config

---
+++ install_everything.sh
@@ -0,0 +1,27 @@
+# Install system level dependencies
+set -exuo pipefail
+apt-get update
+apt-get install --yes software-properties-common build-essential \
+  python python-pip libpython-dev wget unzip zlib1g-dev git
+add-apt-repository --yes ppa:brightbox/ruby-ng
+add-apt-repository --yes "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
+apt-get install --yes ruby2.3 ruby2.3-dev postgresql-9.6 libpq-dev
+gem install bundler
+
+# Set up the database
+/etc/init.d/postgresql start
+sudo -u postgres createuser --superuser root
+/etc/init.d/postgresql stop
+
+# Install application dependencies
+cd $TRAVIS_BUILD_DIR
+bundle install
+pip install -r requirements.txt
+
+# RUN /bin/sh -c 'export PATH="/root/.rbenv/bin:$PATH" && rbenv install 2.3.0'
+# RUN echo 'export PATH="/root/.rbenv/bin:$PATH"' > /etc/profile.d/rbenv
+# RUN echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv
+# ADD . /src
+# RUN /bin/sh -c 'cd /src && source /etc/profile.d/rbenv && gem install bundler && rbenv rehash && bundle install'
