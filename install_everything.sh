8d83d32e6989c60d2aa931d4c58383a3ff04f86b
Tom Dooner <tdooner@codeforamerica.org>
2017-06-17 13:55:32 -0700

    Add sudo in front of commands
    Travis gives this error:
    
    ```
    E: Could not open lock file /var/lib/apt/lists/lock - open (13: Permission denied)
    ```
    
    ...which I think can be worked around with Sudo.

--- install_everything.sh
+++ install_everything.sh
@@ -1,17 +1,17 @@
 # Install system level dependencies
 set -exuo pipefail
-apt-get update
-apt-get install --yes software-properties-common build-essential \
+sudo apt-get update
+sudo apt-get install --yes software-properties-common build-essential \
   python python-pip libpython-dev wget unzip zlib1g-dev git
-add-apt-repository --yes "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-apt-get update
-apt-get install --yes postgresql-9.6 libpq-dev
+sudo add-apt-repository --yes "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install --yes postgresql-9.6 libpq-dev
 
 # Set up the database
-/etc/init.d/postgresql start
+sudo /etc/init.d/postgresql start
 sudo -u postgres createuser --superuser root
-/etc/init.d/postgresql stop
+sudo /etc/init.d/postgresql stop
 
 # Install application dependencies
 cd $TRAVIS_BUILD_DIR
