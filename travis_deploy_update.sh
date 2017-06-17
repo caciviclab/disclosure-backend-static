ac73d331bafab98ac8f0939f1a6e586622a674ac
Tom Dooner <tdooner@codeforamerica.org>
2017-06-17 15:00:06 -0700

    Skip CI on pushed commit
    No sense in running Travis on the pushed commit (else it will create an
    infinite loop.)

--- travis_deploy_update.sh
+++ travis_deploy_update.sh
@@ -5,7 +5,7 @@ deploy() {
   git add build
   git config --global user.name 'Tom Dooner'
   git config --global user.email 'tomdooner@gmail.com'
-  git commit -m 'Run `make clean import process`'
+  git commit -m 'Run `make clean import process`'"\n\n[skip ci]"
   git push --force \
     "https://$GITHUB_AUTH_TOKEN@github.com/caciviclab/disclosure-backend-static.git" \
     HEAD:test_auto_updating \
