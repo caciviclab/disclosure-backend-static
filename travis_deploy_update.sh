e3db0d9b1c27734553d907144e97dd56269b3958
Tom Dooner <tdooner@codeforamerica.org>
2017-06-17 14:48:07 -0700

    Move deployment steps into a separate file
    I'll reorganize all these shell scripts in a little bit. This one
    handles the logic for deploying if necessary.

---
+++ travis_deploy_update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+deploy() {
+  git add build
+  git config --global user.name 'Tom Dooner'
+  git config --global user.email 'tomdooner@gmail.com'
+  git commit -m 'Run `make clean import process`'
+  git push --force \
+    "https://$GITHUB_AUTH_TOKEN@github.com/caciviclab/disclosure-backend-static.git" \
+    HEAD:test_auto_updating \
+    | sed -e "s/$GITHUB_AUTH_TOKEN//"
+}
+
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" -o "${TRAVIS_BRANCH}" = "automatic_updating" ]; then
+  set -x
+  deploy
+else
+  echo "Not deploying since this is not a cron job or on the 'automatic_updating' branch."
+fi
