9a4b724d7a953a5378dfc11373fe07170f98f201
Tom Dooner <tdooner@codeforamerica.org>
2017-06-17 14:06:47 -0700

    Use travis's version of PostgreSQL instead of installing ourselves

--- install_everything.sh
+++ install_everything.sh
@@ -1,12 +1,7 @@
 # Install system level dependencies
 set -exuo pipefail
 sudo apt-get update
-sudo apt-get install --yes software-properties-common build-essential \
-  python python-pip libpython-dev wget unzip zlib1g-dev git
-sudo add-apt-repository --yes "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install --yes postgresql-9.6 libpq-dev
+sudo apt-get install wget unzip zlib1g-dev git
 
 # Set up the database
 sudo /etc/init.d/postgresql start
--- install_stuff_that_comes_with_travis.sh
+++ install_stuff_that_comes_with_travis.sh
@@ -1,5 +1,14 @@
-add-apt-repository --yes ppa:brightbox/ruby-ng
-apt-get update
-apt-get install --yes ruby2.3 ruby2.3-dev
+sudo apt-get install --yes software-properties-common build-essential \
+  python python-pip libpython-dev wget unzip zlib1g-dev git
+sudo add-apt-repository --yes "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo add-apt-repository --yes ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install --yes ruby2.3 ruby2.3-dev postgresql-9.6 libpq-dev
+
+# Set up the database user
+sudo /etc/init.d/postgresql start
+sudo -u postgres createuser --superuser root
+sudo /etc/init.d/postgresql stop
 
 gem install bundler
