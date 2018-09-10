8f2ede4fa09dff0b012f56a2684e021b4c08e0f8
Mike Ubell <mike.ubell@gmail.com>
2018-09-10 15:29:09 -0700

    Generate lists of supporting/opposing expenditures for candidates.

--- calculators/candidate_supporting_expenditures.rb
+++ calculators/candidate_supporting_expenditures.rb
@@ -1,25 +1,22 @@
-# frozen_string_literal: true
-
 # Calculate independent expenditures in support of a candidate.
 class CandidateSupportingExpenditure
   def initialize(candidates: [], ballot_measures: [], committees: [])
-    @committees = committees
     @candidates = candidates
   end
 
   def fetch
     # Get the total indepedent expenditures for candidates by date.
     expenditures = ActiveRecord::Base.connection.execute(<<-SQL)
-      SELECT "Filer_ID", Sum("Amount") as "Total"
+      SELECT "Filer_ID", "Filer_NamL", "Exp_Date", SUM("Amount") as "Total"
       FROM independent_candidate_expenditures
       WHERE "Sup_Opp_Cd" = 'S'
-      GROUP BY "Filer_ID";
+      GROUP BY "Filer_ID", "Filer_NamL", "Exp_Date";
     SQL
 
     total = {}
     # TODO: Key this based off the candidate name rather than the Filer ID, to
     # support IEs for candidates that haven't filed to run yet.
-    expenditures.each_with_object({}) do |row, hash|
+   expenditure_for_candidate = expenditures.each_with_object({}) do |row, hash|
       filer_id = row['Filer_ID'].to_s
       total[filer_id] ||= 0
       total[filer_id] += row['Total']
@@ -29,8 +26,13 @@ class CandidateSupportingExpenditure
     end
 
     @candidates.each do |candidate|
-      filer_id = candidate['FPPC']
-      candidate.save_calculation(:total_supporting_independent, total.fetch(filer_id.to_s, 0).round(2))
+      filer_id = candidate['FPPC'].to_s
+      candidate.save_calculation(:total_supporting_independent, total.fetch(filer_id, 0).round(2))
+
+      sorted =
+        Array(expenditure_for_candidate[filer_id]).sort_by { |row| [row['Filer_NamL'], row['Exp_Date']] }
+
+      candidate.save_calculation(:support_list, sorted)
     end
   end
 end
