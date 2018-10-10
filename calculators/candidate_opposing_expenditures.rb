547fba85057ecf4895ae11ff9641418025da0d4f
Aaron D Borden <adborden@a14n.net>
2018-10-09 21:11:27 -0700

    Include original filer id in IE candidate view

--- calculators/candidate_opposing_expenditures.rb
+++ calculators/candidate_opposing_expenditures.rb
@@ -7,28 +7,28 @@ class CandidateOpposingExpenditure
   def fetch
     # Get the total expenditures against candidates by date.
     expenditures = ActiveRecord::Base.connection.execute(<<-SQL)
-      SELECT "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
+      SELECT candidate_filer_id::varchar, "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
       FROM independent_candidate_expenditures
       WHERE "Sup_Opp_Cd" = 'O'
-      GROUP BY "Filer_ID", "Filer_NamL";
+      GROUP BY candidate_filer_id, "Filer_ID", "Filer_NamL";
     SQL
 
     total = {}
     expenditure_against_candidate = expenditures.each_with_object({}) do |row, hash|
-      filer_id = row['Filer_ID'].to_s
-      total[filer_id] ||= 0
-      total[filer_id] += row['Total']
+      candidate_filer_id = row['candidate_filer_id'].to_s
+      total[candidate_filer_id] ||= 0
+      total[candidate_filer_id] += row['Total']
 
-      hash[filer_id] ||= []
-      hash[filer_id] << row
+      hash[candidate_filer_id] ||= []
+      hash[candidate_filer_id] << row
     end
 
     @candidates.each do |candidate|
-      filer_id = candidate['FPPC'].to_s
-      candidate.save_calculation(:total_opposing, total.fetch(filer_id, 0).round(2))
+      candidate_filer_id = candidate['FPPC'].to_s
+      candidate.save_calculation(:total_opposing, total.fetch(candidate_filer_id, 0).round(2))
 
       sorted =
-        Array(expenditure_against_candidate[filer_id]).sort_by { |row| [row['Filer_NamL'], row['Exp_Date']] }
+        Array(expenditure_against_candidate[candidate_filer_id]).sort_by { |row| [row['Filer_NamL'], row['Exp_Date']] }
 
       candidate.save_calculation(:opposition_list, sorted)
     end
--- calculators/candidate_supporting_expenditures.rb
+++ calculators/candidate_supporting_expenditures.rb
@@ -7,30 +7,30 @@ class CandidateSupportingExpenditure
   def fetch
     # Get the total indepedent expenditures for candidates by date.
     expenditures = ActiveRecord::Base.connection.execute(<<-SQL)
-      SELECT "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
+      SELECT candidate_filer_id::varchar, "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
       FROM independent_candidate_expenditures
       WHERE "Sup_Opp_Cd" = 'S'
-      GROUP BY "Filer_ID", "Filer_NamL";
+      GROUP BY candidate_filer_id, "Filer_ID", "Filer_NamL";
     SQL
 
     total = {}
     # TODO: Key this based off the candidate name rather than the Filer ID, to
     # support IEs for candidates that haven't filed to run yet.
    expenditure_for_candidate = expenditures.each_with_object({}) do |row, hash|
-      filer_id = row['Filer_ID'].to_s
-      total[filer_id] ||= 0
-      total[filer_id] += row['Total']
+      candidate_filer_id = row['candidate_filer_id'].to_s
+      total[candidate_filer_id] ||= 0
+      total[candidate_filer_id] += row['Total']
 
-      hash[filer_id] ||= []
-      hash[filer_id] << row
+      hash[candidate_filer_id] ||= []
+      hash[candidate_filer_id] << row
     end
 
     @candidates.each do |candidate|
-      filer_id = candidate['FPPC'].to_s
-      candidate.save_calculation(:total_supporting_independent, total.fetch(filer_id, 0).round(2))
+      candidate_filer_id = candidate['FPPC'].to_s
+      candidate.save_calculation(:total_supporting_independent, total.fetch(candidate_filer_id, 0).round(2))
 
       sorted =
-        Array(expenditure_for_candidate[filer_id]).sort_by { |row| [row['Filer_NamL'], row['Exp_Date']] }
+        Array(expenditure_for_candidate[candidate_filer_id]).sort_by { |row| [row['Filer_NamL'], row['Exp_Date']] }
 
       candidate.save_calculation(:support_list, sorted)
     end
