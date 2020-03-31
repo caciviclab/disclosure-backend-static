61a6c223a2fc054c2ff2e7b69d9e6ca4f36505b7
Gaurav Kulkarni <gaurav@gauravkulkarni.com>
2020-03-31 00:42:03 -0700

    Add data key to election markdown

--- process.rb
+++ process.rb
@@ -83,6 +83,7 @@ ELECTIONS.each do |election_name, election|
   office_elections_by_label = office_elections.group_by(&:label)
   election_content = YAML.dump(
     'title' => election[:title],
+    'data_key' => election[:name],
     'locality' => locality,
     'election' => election[:date].to_s,
     'office_elections' => office_elections_by_label.map do |label, items|
