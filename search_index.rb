cbc66bf2d79c9d9cc8a6a419e181f559e4175697
Michael Ubell <mike.ubell@gmail.com>
2020-08-01 17:24:46 -0700

    Add info for Independent Expenditure Committees

--- search_index.rb
+++ search_index.rb
@@ -11,13 +11,26 @@ require_relative './environment.rb'
 
 require 'algoliasearch'
 require 'optparse'
-options = { skip_contributors: false }
-OptionParser.new do |opts|
-  opts.banner = "Usage: #{$0} [options]"
-  opts.on('--skip-contributors', 'Skip indexing contributor list') do |s|
-    options[:skip_contributors] = true
+
+def getContributors(name, id, election)
+  contrib = Committee.where(["\"Filer_ID\" = ?", id]).first
+    .calculation(:contribution_list)
+  return nil if contrib.nil?
+  return contrib.map do |contributor|
+    {
+      type: :contributor,
+      first_name: contributor['Tran_NamF'],
+      last_name: contributor['Tran_NamL'],
+      amount: contributor['Tran_Amt1'],
+      committee_name: name,
+      committee_id: id,
+      election_slug: election.name,
+      election_location: election.location,
+      election_date: election.date,
+      election_title: election.title,
+    }
   end
-end.parse!
+end
 
 client = Algolia::Client.new(
   application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'],
@@ -46,35 +59,70 @@ candidate_data = Candidate.includes(:election, :office_election).map do |candida
 end
 puts "Indexing #{candidate_data.length} Candidates..."
 
+iec_data = []
+iec_contrib  = []
 contributor_data = []
-if options[:skip_contributors]
-  puts "Skipping contributor indexing..."
-else
-  Candidate.includes(:election, :committee, :office_election).find_each do |candidate|
-    next if candidate.committee.nil?
-    list = candidate.committee.calculation(:contribution_list).map do |contributor|
-      {
-        type: :contributor,
-        first_name: contributor['Tran_NamF'],
-        last_name: contributor['Tran_NamL'],
-        amount: contributor['Tran_Amt1'],
-        name: candidate['Candidate'],
-        candidate_slug: slugify(candidate['Candidate']),
-        office_label: candidate.office_election.label,
-        office_title: candidate.office_election.title,
-        office_slug: slugify(candidate.office_election.title),
-        election_slug: candidate.election.name,
-        election_location: candidate.election.location,
-        election_date: candidate.election.date,
-        election_title: candidate.election.title,
-      }
-    end
-    unless list.nil?
-      contributor_data += list
+Candidate.includes(:election, :committee, :office_election).find_each do |candidate|
+  next if candidate.committee.nil?
+  list = candidate.committee.calculation(:contribution_list).map do |contributor|
+    {
+      type: :contributor,
+      first_name: contributor['Tran_NamF'],
+      last_name: contributor['Tran_NamL'],
+      amount: contributor['Tran_Amt1'],
+      name: candidate['Candidate'],
+      candidate_slug: slugify(candidate['Candidate']),
+      office_label: candidate.office_election.label,
+      office_title: candidate.office_election.title,
+      office_slug: slugify(candidate.office_election.title),
+      election_slug: candidate.election.name,
+      election_location: candidate.election.location,
+      election_date: candidate.election.date,
+      election_title: candidate.election.title,
+    }
+  end
+  unless list.nil?
+    contributor_data += list
+  end
+
+  [
+    [:support_list, "Supporting"],
+    [:opposition_list, "Opposing"],
+  ].each do |calculation, supporting|
+    list = candidate.calculation(calculation)
+    next if list.nil?
+    list.each do |iec|
+      iec_data +=
+        [{
+          type: :iec,
+          committee_name: iec['Filer_NamL'],
+          committee_id: iec['Filer_ID'],
+          supporting: supporting,
+          amount: iec['Total'],
+          name: candidate['Candidate'],
+          candidate_slug: slugify(candidate['Candidate']),
+          office_label: candidate.office_election.label,
+          office_title: candidate.office_election.title,
+          office_slug: slugify(candidate.office_election.title),
+          election_slug: candidate.election.name,
+          election_location: candidate.election.location,
+          election_date: candidate.election.date,
+          election_title: candidate.election.title,
+        }]
+      contrib = getContributors(iec['Filer_NamL'], iec['Filer_ID'], candidate.election)
+      next if contrib.nil?
+      iec_contrib += contrib
     end
   end
-  puts "Indexing #{contributor_data.length} Contributors..."
 end
+
+# A committee can support/oppose multiple candidates
+# Delete duplicates, not too efficient but keeps the code simple.
+iec_contrib.uniq!
+
+puts "Indexing #{contributor_data.length} Contributors..."
+puts "Indexing #{iec_data.length} Independent Committies..."
+puts "Indexing #{iec_contrib.length} IC Contributors..."
 
 referendum_data = []
 ballot_committees = []
@@ -112,23 +160,9 @@ Referendum.includes(:election).find_each do |referendum|
         election_date: referendum.election.date,
         election_title: referendum.election.title,
       }]
-      contrib = Committee.where(["\"Filer_ID\" = ?", committee['id']]).first
-        .calculation(:contribution_list)
+      contrib = getContributors(committee['name'], committee['id'], referendum.election)
       next if contrib.nil?
-      ballot_contrib += contrib.map do |contributor|
-        {
-          type: :contributor,
-          first_name: contributor['Tran_NamF'],
-          last_name: contributor['Tran_NamL'],
-          amount: contributor['Tran_Amt1'],
-          committee_name: committee['name'],
-          committee_id: committee['id'],
-          election_slug: referendum.election.name,
-          election_location: referendum.election.location,
-          election_date: referendum.election.date,
-          election_title: referendum.election.title,
-        }
-      end
+      ballot_contrib += contrib
     end
   end
 end
@@ -136,7 +170,7 @@ puts "Indexing #{referendum_data.length} Referendums..."
 puts "Indexing #{ballot_committees.length} Ballot Committees..."
 puts "Indexing #{ballot_contrib.length} Ballot Contributors..."
 
-all_data =
-  ballot_contrib + ballot_committees + referendum_data + contributor_data + candidate_data
+all_data = ballot_contrib + ballot_committees + referendum_data +
+  contributor_data + candidate_data + iec_data + iec_contrib
 puts "total records: #{all_data.length}"
 index.replace_all_objects(all_data)
