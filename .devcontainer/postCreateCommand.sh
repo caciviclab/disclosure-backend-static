d44d9849c25bbb31229568a8a2ab15cbba0cccf3
Chenglim Ear <chenglim_ear@yahoo.com>
2023-10-30 21:50:41 -0700

    add download requirements to dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,5 +3,6 @@
 pip install --upgrade pip
 #pip install 'urllib3[secure]'
 pip install -r requirements.txt
+pip install -r download/requirements.txt
 sudo gem install pg bundler
 sudo bundle install
