f08c7c170634531bd4d3de02f66144d165e82b8f
Chenglim Ear <chenglim_ear@yahoo.com>
2023-11-01 09:03:58 -0700

    remove unused script

--- .devcontainer/enable-ssh-agent.ps1
+++
@@ -1,5 +0,0 @@
-# To use git in the dev container, you have to enable ssh agent on Windows.  Try running the following in a PowerShell as Administrator.
-# Make sure you're running as an Administrator
-Set-Service ssh-agent -StartupType Automatic
-Start-Service ssh-agent
-Get-Service ssh-agent
