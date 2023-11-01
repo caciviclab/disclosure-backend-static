5ad4e610a6ee8aa90a03374c97110247b9aab009
Chenglim Ear <chenglim_ear@yahoo.com>
2023-10-31 20:29:49 -0700

    test getting dev container to build

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 pip install -r download/requirements.txt
 sudo gem install pg bundler
 sudo bundle install
+
--- .github/post-action-command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#chmod a+x .devcontainer/post-create-command.sh
-#.devcontainer/post-create-command.sh
-
-python test_pull_from_gdrive.py
