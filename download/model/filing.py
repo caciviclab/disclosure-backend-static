9f49321000cb0587ab1bc8e3aa6e5b4d72f49e88
ckb <ckingbailey@gmail.com>
2023-11-16 21:19:58 -0800

    filings works with polars

--- download/model/filing.py
+++ download/model/filing.py
@@ -1,4 +1,5 @@
 """ Filings """
+from polars import UInt64, Utf8
 from .base import BaseModel
 
 class Filings(BaseModel):
@@ -6,8 +7,8 @@ class Filings(BaseModel):
     def __init__(self, filings):
         super().__init__([
             {
-                'filing_nid': f['filingNid'],
-                'filer_nid': f['filerMeta']['filerId'],
+                'filing_nid': f['filingNid'], # filingNid is an UUID, with dashes, like 4f6949c8-3765-484e-8e74-050559c077c5
+                'filer_nid': int(f['filerMeta']['filerId']), # filerId is an int
                 'Report_Num': f['filingMeta']['amendmentSequence'],
                 'Rpt_Date': f['filingMeta']['legalFilingDate'],
                 'From_Date': f['filingMeta']['startDate'],
@@ -22,4 +23,14 @@ class Filings(BaseModel):
             'Rpt_Date': 'string',
             'From_Date': 'string',
             'Thru_Date': 'string'
+        }
+
+        self._pl_dtypes = {
+            'filing_nid': Utf8,
+            'filer_nid': UInt64,
+            'Report_Num': UInt64,
+            'Rpt_Date': Utf8,
+            'From_Date': Utf8,
+            'Thru_Date': Utf8
+
         }
