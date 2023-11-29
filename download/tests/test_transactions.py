8de7e38421654b325c3af3fe805895fddec4921b
ckb <ckingbailey@gmail.com>
2023-11-28 21:36:07 -0800

    a simple test on transactions

---
+++ download/tests/test_transactions.py
@@ -0,0 +1,13 @@
+from model.transaction import DTYPES, Transactions
+
+def test_transactions_does_not_raise(transactions_json):
+    ''' Just check that it doesn't error '''
+    Transactions(transactions_json)
+
+def test_transactions_contains_expected_fields(transactions_df):
+    ''' Check that Transactions has expected fields '''
+    expect_cols = list(DTYPES.keys())
+
+    actual_cols = transactions_df.columns
+
+    assert sorted(actual_cols) == sorted(expect_cols)
