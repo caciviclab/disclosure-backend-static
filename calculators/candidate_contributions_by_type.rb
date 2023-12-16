0e76c9fe3fef20985cfdf640e9f690838ca57dea
Michael Ubell <mike.ubell@gmail.com>
2023-12-15 16:00:26 -0800

    Fix #399: include nonmonitary unitemized contributions

--- calculators/candidate_contributions_by_type.rb
+++ calculators/candidate_contributions_by_type.rb
@@ -101,7 +101,8 @@ class CandidateContributionsByType
       results = ActiveRecord::Base.connection.execute <<-SQL
         SELECT "Filer_ID", SUM("Amount_A") AS "Amount_A" FROM "Summary"
         WHERE "Filer_ID" IN ('#{@candidates_by_filer_id.keys.join "','"}')
-          AND "Form_Type" = 'A' AND "Line_Item" = '2'
+          AND ( "Form_Type" = 'A' OR "Form_Type" = 'C')
+          AND "Line_Item" = '2'
         GROUP BY "Filer_ID"
         ORDER BY "Filer_ID"
       SQL
