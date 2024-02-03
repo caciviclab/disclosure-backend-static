0d3d07317534326284bc95e3164529397b9e7e2a
ChenglimEar <ChenglimEar@users.noreply.github.com>
2024-02-03 12:51:52 -0800

    fix calculators to handle candidates in multiple elections (#333)
    * split contributions by type to multiple elections when a candidate was
    in multiple elections
    
    * removed commented code
    
    * create candidate_summary view to associate "Summary" info with specific
    election
    
    * add total contributions to digest.json
    
    * use hash of hash for contributions by type
    
    * add total contributions by type and source to digests
    
    * take election into account when calculating total contributions and
    contributions by source
    
    * organize totals calculated from various sources in digests.json
    
    * update digests.json to include more totals
    
    * calculate contribution totals for all tickets (candidates and referendums) combined
    
    * add more totals to digest and separate by contributions vs expenditures
    vs loans
    
    * update expenditures to be split on election and other calculations to
    take election into account
    
    * revert committee contribution list calculator
    
    * some comments about the totals calculated for digests.json
    
    * update digests to only show totals that we want to compare
    
    * add loans to total for contributions by type and origin
    
    * move totals logic out of main
    
    * remove build directory for reset
    
    * add build directory back
    
    * re-run
    
    * switch total expenditures calculator to use new candidate_summary view
    which joins Summary with candidates using the from and thru dates
    instead of the report date. This provides consistency and so if we
    decide to use the report date instead for the join, we change it in the
    view and consistently apply it everywhere.
    
    * remove build directory
    
    * add generated build directory
    
    * add report on candidate totals
    
    * attempt to get python 3.9 to be used
    
    * don't use sudo for pip install
    
    * fix calculator for candidate expenditures by type to correctly replace type short name with long name
    
    * fix calculation for supporting_by_type
    
    * fix null values for total_supporting_independent
    
    * fix null values for opposing_expenditures
    
    * remove build dir to reset
    
    * update build
    
    * removing build dir to reset
    
    * regenerate build directory
    
    * reset build dir
    
    * Include information about bin/report-candidates.py in README.md
    
    * add regenerated build dir

--- calculators/candidate_supporting_expenditures.rb
+++ calculators/candidate_supporting_expenditures.rb
@@ -9,32 +9,40 @@ class CandidateSupportingExpenditure
     # The Filer_NamL can be different in different records for the
     # same Filer_ID.
     expenditures = ActiveRecord::Base.connection.execute(<<-SQL)
-      SELECT "Cand_ID", "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
+      SELECT election_name, "Cand_ID", "Filer_ID", "Filer_NamL", SUM("Amount") as "Total"
       FROM independent_candidate_expenditures
       WHERE "Sup_Opp_Cd" = 'S'
-      GROUP BY "Cand_ID", "Filer_ID", "Filer_NamL"
+      GROUP BY election_name, "Cand_ID", "Filer_ID", "Filer_NamL"
     SQL
 
     total = {}
     # TODO: Key this based off the candidate name rather than the Filer ID, to
     # support IEs for candidates that haven't filed to run yet.
    expenditure_for_candidate = expenditures.each_with_object({}) do |row, hash|
+      election_name = row['election_name']
       filer_id = row['Cand_ID'].to_s
-      total[filer_id] ||= 0
-      total[filer_id] += row['Total']
+      total[election_name] ||= {}
+      total[election_name][filer_id] ||= 0
+      total[election_name][filer_id] += row['Total']
 
-      hash[filer_id] ||= []
-      hash[filer_id] << row
+      hash[election_name] ||= {}
+      hash[election_name][filer_id] ||= []
+      hash[election_name][filer_id] << row
     end
 
     @candidates.each do |candidate|
+      election_name = candidate['election_name']
       filer_id = candidate['FPPC'].to_s
-      candidate.save_calculation(:total_supporting_independent, total.fetch(filer_id, 0).round(2))
+      election_total = total[election_name]
+      if !election_total.nil?
+        total_supporting_independent = election_total.fetch(filer_id, 0)
+        candidate.save_calculation(:total_supporting_independent, total_supporting_independent.round(2))
 
-      sorted =
-        Array(expenditure_for_candidate[filer_id]).sort_by { |row| [row['Filer_NamL'], row['Expn_Date']] }
+        sorted =
+          Array(expenditure_for_candidate[election_name][filer_id]).sort_by { |row| [row['Filer_NamL'], row['Expn_Date']] }
 
-      candidate.save_calculation(:support_list, sorted)
+        candidate.save_calculation(:support_list, sorted)
+      end
     end
   end
 end
--- calculators/total_expenditures_calculator.rb
+++ calculators/total_expenditures_calculator.rb
@@ -2,32 +2,36 @@ class TotalExpendituresCalculator
   def initialize(candidates: [], ballot_measures: [], committees: [])
     @candidates_by_filer_id =
       candidates.where('"FPPC" IS NOT NULL').index_by { |c| c.FPPC }
+    @candidates_by_election_filer_id =
+      candidates.find_all { |c| c.FPPC.present? }.group_by { |row| row.election_name }.transform_values do |values|
+        values.index_by { |c| c.FPPC.to_s }
+      end
   end
 
   def fetch
     results = ActiveRecord::Base.connection.execute <<-SQL
-      SELECT "Filer_ID", SUM("Amount_A") AS "Amount_A"
-      FROM "Summary", candidates
-      WHERE "Filer_ID" = "FPPC"::varchar
-      AND "Form_Type" = 'F460'
+      SELECT election_name, "Filer_ID", SUM("Amount_A") AS "Amount_A"
+      FROM candidate_summary
+      WHERE "Form_Type" = 'F460'
       AND "Line_Item" = '11'
-      AND ("Start_Date" IS NULL OR "Rpt_Date" >= "Start_Date")
-      AND ("End_Date" IS NULL OR "Rpt_Date" <= "End_Date")
-      GROUP BY "Filer_ID"
-      ORDER BY "Filer_ID"
+      GROUP BY election_name, "Filer_ID"
+      ORDER BY election_name, "Filer_ID"
     SQL
 
     late_expenditures = ActiveRecord::Base.connection.execute <<-SQL
-      SELECT "Filer_ID", SUM("Amount") AS "Amount_A"
-      FROM "497"
+      SELECT election_name, "Filer_ID", SUM("Amount") AS "Amount_A"
+      FROM candidate_497
       WHERE "Filer_ID" IN ('#{@candidates_by_filer_id.keys.join "', '"}')
       AND "Form_Type" = 'F497P2'
-      GROUP BY "Filer_ID"
-      ORDER BY "Filer_ID"
+      GROUP BY election_name, "Filer_ID"
+      ORDER BY election_name, "Filer_ID"
     SQL
 
     (results.to_a + late_expenditures.to_a).each do |result|
-      candidate = @candidates_by_filer_id[result['Filer_ID'].to_i]
+      election_name = result['election_name']
+      filer_id = result['Filer_ID'].to_s
+
+      candidate = @candidates_by_election_filer_id[election_name][filer_id]
       candidate.save_calculation(:total_expenditures, result['Amount_A'].round(2))
     end
   end
