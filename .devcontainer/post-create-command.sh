0e6cca517c9f27e12b143b782a70c10f9f7c67b2
Chenglim Ear <chenglim_ear@yahoo.com>
2024-07-03 03:06:46 +0000

    add post-create-command.sh back

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+pip install --upgrade pip
+#pip install 'urllib3[secure]'
+pip install -r requirements.txt
+pip install -r download/requirements.txt
+sudo gem install pg bundler
+sudo bundle install
