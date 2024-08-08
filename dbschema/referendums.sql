19088d0c9f91dd5e561410d6d23b4d4fd3c96eaf
Michael Ubell <mike.ubell@gmail.com>
2024-08-07 18:32:47 -0700

    update referendums.sql

--- dbschema/referendums.sql
+++ dbschema/referendums.sql
@@ -2,7 +2,7 @@ CREATE TABLE referendums (
 	election_name VARCHAR(18) NOT NULL, 
 	"Measure_number" VARCHAR(4) NOT NULL, 
 	"Short_Title" VARCHAR(128) NOT NULL, 
-	"Full_Title" VARCHAR(328), 
+	"Full_Title" VARCHAR(1024), 
 	"Summary" VARCHAR(2048), 
 	"VotersEdge" VARCHAR(129), 
 	"Internal_notes" VARCHAR(1024), 
