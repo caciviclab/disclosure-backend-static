2422f273d1a03b771b0aedc73165486040805d9a
OpenDisclosure Deploybot <open-disclosure@gmail.com>
2026-06-29 02:29:27 -0700

    Run `make clean download import process`
    This is an automated update by travis-ci at
    Mon Jun 29 02:29:27 AM PDT 2026
    
    [skip ci]

--- build/schema.sql
+++ build/schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict dc57rGNdws8buEAgI5hLsbrYqqajfPnZuBaVn59wTW82kmB1x3MBXgEPe4b08CR
+\restrict qJJGww3V4sJBt4OlXES7bhLMZMPV8TzGCXvpo9mf2nXTn4AL64E92zJG1CHkLrw
 
 -- Dumped from database version 14.19 (Ubuntu 14.19-1.pgdg22.04+1)
 -- Dumped by pg_dump version 14.19 (Ubuntu 14.19-1.pgdg22.04+1)
@@ -2094,5 +2094,5 @@ ALTER TABLE ONLY public.referendums
 -- PostgreSQL database dump complete
 --
 
-\unrestrict dc57rGNdws8buEAgI5hLsbrYqqajfPnZuBaVn59wTW82kmB1x3MBXgEPe4b08CR
+\unrestrict qJJGww3V4sJBt4OlXES7bhLMZMPV8TzGCXvpo9mf2nXTn4AL64E92zJG1CHkLrw
 
